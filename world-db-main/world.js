68b1a9b47bdbc94d7d1def9b3ec5d3fa653b3b4c
Olina Marie <olina.marie@my.fiveislands.uwi.edu>
2023-11-16 20:15:09 -0800

    Added and Edited world.js

---
+++ world-db-main/world.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('lookup').addEventListener('click', function () {
+
+        var countryInput = document.getElementById('country');
+        var country = countryInput.value;
+        var xhr = new XMLHttpRequest();
+
+        if (country.trim() !== '') {
+            xhr.open('GET', 'world.php?country=' + encodeURIComponent(country), true);
+        } else {
+            xhr.open('GET', 'world.php', true);
+        }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('result').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.send();
+    });
+});
+
