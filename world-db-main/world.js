1aa9a362ecbb189369947e3763aaeea004187cf4
Olina Marie <olina.marie@my.fiveislands.uwi.edu>
2023-11-19 20:33:48 -0800

    Edited Javascript code

--- world-db-main/world.js
+++ world-db-main/world.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('lookup').addEventListener('click', function () {
-
         var countryInput = document.getElementById('country');
         var country = countryInput.value;
         var xhr = new XMLHttpRequest();
-
         if (country.trim() !== '') {
             xhr.open('GET', 'world.php?country=' + encodeURIComponent(country), true);
         } else {
             xhr.open('GET', 'world.php', true);
+        }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById('result').innerHTML = xhr.responseText;
+            }
+        };
+        xhr.send();
+    });
+
+    document.getElementById('lookupCities').addEventListener('click', function () {
+
+        var countryInput = document.getElementById('country');
+        var country = countryInput.value;
+        var xhr = new XMLHttpRequest();
+        if (country.trim() !== '') {
+            xhr.open('GET', 'world.php?country=' + encodeURIComponent(country) + '&lookup=cities', true);
         }
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
